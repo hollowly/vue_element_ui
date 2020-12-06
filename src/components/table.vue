22f8c2c9306ae22770970ae5bdd5279ac5389911
hollowly <2499538506@qq.com>
2020-12-06 13:45:59 +0800

    enter

--- src/components/table.vue
+++ src/components/table.vue
@@ -36,7 +36,52 @@
 						<el-progress type="circle" :percentage="50" status="exception"></el-progress>
 					</div>
 
+					<h3>树形控件</h3>
+					<el-tree :data="data1" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+				
+					<div>
+						<el-badge :value="12" class="itema">
+							<el-button size="small">评论</el-button>
+						</el-badge>
+						<el-badge :value="3" class="itema">
+							<el-button size="small">回复</el-button>
+						</el-badge>
 
+						<el-dropdown trigger="click" class="itema">
+							<span class="el-dropdown-link">
+								点我查看<i class="el-icon-caret-bottom el-icon--right"></i>
+							</span>
+							<el-dropdown-menu slot="dropdown">
+								<el-dropdown-item class="clearfix">
+									评论
+									<el-badge class="mark" :value="12" />
+								</el-dropdown-item>
+								<el-dropdown-item class="clearfix">
+									回复
+									<el-badge class="mark" :value="3" />
+								</el-dropdown-item>
+							</el-dropdown-menu>
+						</el-dropdown>
+
+						<el-badge is-dot class="itema">数据查询</el-badge>
+						<el-badge is-dot class="itema">
+							<el-button class="share-button" icon="el-icon-share" type="primary"></el-button>
+						</el-badge>
+
+
+					</div>
+		
+					<div class="block">
+						<h3>分页</h3>
+						<el-pagination
+							@size-change="handleSizeChange"
+							@current-change="handleCurrentChange"
+							:current-page.sync="currentPage1"
+							:page-size="10"
+							layout="prev, pager, next, jumper"
+							:total="1000">
+						</el-pagination>
+					</div>
 
 				</el-card>
 			</el-col>
@@ -83,12 +128,61 @@
           { name: '标签三', type: 'info' },
           { name: '标签四', type: 'warning' },
           { name: '标签五', type: 'danger' }
-        ]
+				],
+				data1: [{
+          label: '一级 1',
+          children: [{
+            label: '二级 1-1',
+            children: [{
+              label: '三级 1-1-1'
+            }]
+          }]
+        }, {
+          label: '一级 2',
+          children: [{
+            label: '二级 2-1',
+            children: [{
+              label: '三级 2-1-1'
+            }]
+          }, {
+            label: '二级 2-2',
+            children: [{
+              label: '三级 2-2-1'
+            }]
+          }]
+        }, {
+          label: '一级 3',
+          children: [{
+            label: '二级 3-1',
+            children: [{
+              label: '三级 3-1-1'
+            }]
+          }, {
+            label: '二级 3-2',
+            children: [{
+              label: '三级 3-2-1'
+            }]
+          }]
+        }],
+        defaultProps: {
+          children: 'children',
+          label: 'label'
+				},
+        currentPage1: 2
       }
 		},
     methods: {
       handleClick(row) {
         console.log(row);
+			},
+			 handleNodeClick(data) {
+        console.log(data);
+			},
+			 handleSizeChange(val) {
+        console.log(`每页 ${val} 条`);
+      },
+      handleCurrentChange(val) {
+        console.log(`当前页: ${val}`);
       }
     },
   }
@@ -106,5 +200,12 @@
 	}
 	.tag {
 		margin: 0px 5px 0px 5px;
+	}
+	h3 {
+		margin: 20px 0px 20px 0px;
+	}
+	.itema {
+		display: inline-block;
+		margin-right:30px !important;
 	}
 </style>
