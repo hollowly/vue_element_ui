a17d1fb873583cc13766da5a365622b672183896
hollowly <2499538506@qq.com>
2020-12-06 14:18:04 +0800

    enter

--- src/components/navigation.vue
+++ src/components/navigation.vue
@@ -1,22 +1,78 @@
 <!--  -->
 <template>
 	<div>
-		Navigation
+		<h1>导航菜单navigation</h1>
+		<el-row type='flex' justify="center">
+			<el-col :span='14'>
+				<el-card>
+					<el-menu :default-active="activeIndex2" class="el-menu-demo" mode="horizontal"
+						@select="handleSelect" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
+						<el-menu-item index="1">处理中心</el-menu-item>
+						<el-submenu index="2">
+							<template slot="title">我的工作台</template>
+							<el-menu-item index="2-1">选项1</el-menu-item>
+							<el-menu-item index="2-2">选项2</el-menu-item>
+							<el-menu-item index="2-3">选项3</el-menu-item>
+						</el-submenu>
+						<el-menu-item index="3"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
+					</el-menu>
+
+					  <el-col :span="12">
+							<h5>自定义颜色</h5>
+							<el-menu default-active="2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose"
+								background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
+								<el-submenu index="1">
+									<template slot="title">
+										<i class="el-icon-location"></i>
+										<span>导航一</span>
+									</template>
+									<el-menu-item-group>
+										<template slot="title">分组一</template>
+										<el-menu-item index="1-1">选项1</el-menu-item>
+										<el-menu-item index="1-2">选项2</el-menu-item>
+									</el-menu-item-group>
+									<el-menu-item-group title="分组2">
+										<el-menu-item index="1-3">选项3</el-menu-item>
+									</el-menu-item-group>
+									<el-submenu index="1-4">
+										<template slot="title">选项4</template>
+										<el-menu-item index="1-4-1">选项1</el-menu-item>
+									</el-submenu>
+								</el-submenu>
+								<el-menu-item index="2">
+									<i class="el-icon-menu"></i>
+									<span slot="title">导航二</span>
+								</el-menu-item>
+								<el-menu-item index="3">
+									<i class="el-icon-setting"></i>
+									<span slot="title">导航三</span>
+								</el-menu-item>
+							</el-menu>
+						</el-col>
+				</el-card>
+			</el-col>
+		</el-row>
 	</div>
 </template>
 
 <script>
-export default {
-	data () {
-		return {
-
-		}
-	},
-
-}
-
+  export default {
+    data() {
+      return {
+        activeIndex: '1',
+        activeIndex2: '1'
+      };
+    },
+    methods: {
+      handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
+  }
 </script>
 
 <style scoped>
-
+	h1 {
+		text-align: center;
+	}
 </style>
